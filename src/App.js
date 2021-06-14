c9bb0d43f99d2ed264b58161bf594c609dd6a910
Rui Costa <aluno19786@ipt.pt>
2021-06-14 23:01:55 +0100

    Commit feito no VSCode

--- src/App.js
+++ src/App.js
@@ -4,29 +4,41 @@
 
 
 import React from "react";
-import Tabela from './Tabela';
-import Tabela2 from './Tabela2';
+//import Tabela from './Tabela';
+//import Tabela2 from './Tabela2';
+import Tabela4 from './Tabela4';
 
 /**Componente principal do meu projecto
  * e é uma classe de react
  */
 class App extends React.Component {
 
+  state = {
+    alunos : [
+      {"nome":"Maria",
+      "apelido":"Caldeirão"},
+      {"nome":"Mário",
+      "apelido":"Silva"},
+      {"nome":"João",
+      "apelido":"Castro"},
+      {"nome":"Carlos",
+      "apelido":"Costa"}
+    ]
+  }
+
+
   render() {
     //este método é obrigatório caso se trate de uma classe
+    const {alunos} = this.state;
+
     return(
       <div className="container">
         <h1>Lista de Alunos</h1>
         <div className="row">
           <div className="col-4">
-            <h4>Tabela normal</h4>
-            <Tabela />
-
-          </div>
-          <div className="col-4">
-            <h4>Tabela construída com componentes</h4>
-            <Tabela2 />
-
+            <h4>Tabela 4 construída com componentes</h4>
+            <Tabela4 parDadosAlunos={alunos}/>
+            {/* parDadosAlunos é o parâmetro de input que existirá na Tabela3 */}
           </div>
         </div>
       </div>
---
+++ src/Tabela4.js
@@ -0,0 +1,66 @@
+
+//TAbela.js
+
+import React from "react";
+
+
+function CabecalhoTabela() {
+    return (
+        <thead>
+            <tr>
+            <th>Nome</th>
+            <th>Apelido</th>
+            </tr>
+        </thead>
+    );
+}
+
+/**Componente que representa o Corpo da Tabela
+ * Isto é uma arrow function
+ */
+const CorpoTabela =(props)=>{
+
+    //o map funciona como um foreach que irá iterar todos os items dos dados
+    const linhas=props.dadosAlunos.map((linha, index) =>  {
+      return(
+        <tr key={index}>
+          <td>{linha.nome}</td>
+          <td>{linha.apelido}</td>
+          <td>apagar</td>
+        </tr>
+      );
+    });
+
+    return (
+      <tbody> {linhas} </tbody>
+
+    );
+}
+
+
+
+
+
+class Tabela4 extends React.Component {
+
+
+    render() {
+
+        const {parDadosAlunos}=this.props;
+
+
+        return (
+            <table className="table">
+              
+              <CabecalhoTabela/>
+              <CorpoTabela dadosAlunos={parDadosAlunos} />
+
+            </table>
+
+        );
+    };
+
+}
+
+export default Tabela4;
+
